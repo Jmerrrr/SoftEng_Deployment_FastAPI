9d6644e858148fc43aeb11b0320d667ce194a655
Sarcophax <135771114+Sarcophax@users.noreply.github.com>
2024-01-08 13:35:21 +0800

    Changes in Embeddings and added zero shot clissification function

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import copy
 import os
 import warnings
 
+import requests
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI, Form, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from langchain.chains import RetrievalQA
+from langchain.chains import LLMChain, RetrievalQA
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
 from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
@@ -23,13 +24,15 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 load_dotenv()
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
-embeddings = HuggingFaceInferenceAPIEmbeddings(api_key=os.getenv("HUGGINGFACEHUB_API_TOKEN"),model_name="sentence-transformers/all-MiniLM-l6-v2")
+embeddings = HuggingFaceInferenceAPIEmbeddings(api_key=os.getenv("HUGGINGFACEHUB_API_TOKEN"),model_name="sentence-transformers/all-mpnet-base-v2")
 
 load_vector_store = Chroma(persist_directory = "stores/stored_cosine",embedding_function = embeddings)
 retriever = load_vector_store.as_retriever(search_kwargs={"k":2})
 
+# ChatBot LLM
 repo_id = "MBZUAI/LaMini-Flan-T5-783M"
 llm = HuggingFaceHub(repo_id=repo_id,model_kwargs={"temperature":0.3,"max_length":500})
+
 
 prompt_template="""
 Use the following pieces of information to answer the user's question.
@@ -60,27 +63,103 @@ def qa_chain():
 
 query = qa_chain()
 
-def process_answer(prompt:str):
+zs_template = r"""
+Reply in cheerful tone. 
+Human: {text}
+If you don't know the answer, just say the you don't know, don't try to make up an answer.
+Only return the helpful answer below and nothing else.
+Helful answer:
+"""
+zs_prompt = PromptTemplate.from_template(template = zs_template, zs_input_variables=["text"])
+
+conversation = LLMChain(prompt=zs_prompt, llm = llm)
+
+API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-mnli"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+# Zero_shot LLM
+def zero_shot_handler(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+def zero_shot(userPrompt):
+
+    classifier = zero_shot_handler({
+        "inputs":userPrompt,
+        "parameters":{"candidate_labels": ["Simple Conversation", "School related question"]}
+        })
+
+    if classifier["labels"][0] == "Simple Conversation":
+        final_result = conversation.run({"text":userPrompt})
+        return final_result
+    if classifier["labels"][0] == "School related question":
+        final_result = query(userPrompt)
+        return final_result["result"]
+
+def process_answer(prompt):
     question = prompt
-    qa= query(question)
-    answer = qa["result"]
+    qa= zero_shot(question)
+    answer = qa
     return answer
 
-def clear_memory():
-    memory.clear()
+print("Start: ")
+while True:
+    print("-> ")
+    userInput = input("")
+    if userInput == "bye":
+        print("END")
+        break
 
-@app.get("/")
-def form_post(request: Request):
-    results = "What is Your Query"
-    return templates.TemplateResponse('index.html', context={'request': request, 'results': results})
- 
-@app.get('/lamini')
-def model(question : str):
-    res = process_answer(question)
-    result = copy.deepcopy(res)
-    return result
+    else:
+        answer = process_answer(userInput)
+        # answer = qa(query)
+        # print(answer["result"])
+        print(answer)
+# def clear_memory():
+#     memory.clear()
+#
+# @app.get("/")
+# def form_post(request: Request):
+#     results = "What is Your Query"
+#     return templates.TemplateResponse('index.html', context={'request': request, 'results': results})
+#  
+# @app.get('/lamini')
+# def model(question : str):
+#     res = process_answer(question)
+#     result = copy.deepcopy(res)
+#     return result
+#
+# @app.get('/clearMem')
+# def clearMemory():
+#     clear_memory()
 
-@app.get('/clearMem')
-def clearMemory():
-    clear_memory()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
